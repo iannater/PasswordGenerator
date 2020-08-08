094eabd1f86cbcb908140971144e2f82a99085df
Ian Nater <ian.nater@gmail.com>
2020-08-07 22:11:41 -0500

    added comments to the java file and changed the coloring of the button, also finishing the if statement for lowercase and symbol

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,45 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-
+//Created these variables and arrays in order to allow users to select different password options.
 let upperCase = ["A","B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z","Y","Z"];
-let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let symbol  = ["!","@", "#","$","%","^","&","*","(",")","_","+"];
 let number = ["1","2","3","4","5","6","7","8","9","0"];
-let length = ""
 
 
 
 function generatePassword(){
-var result = [];
+  var result = [];
   //store characters to include in password
   var possibleChar=[];
   //contain one of ea.  chosen character, to ensure  use
   var guranteeChar =[];
 
-
+  // Asking the user how many characters they would like their password to be.
   var length = parseInt(
     prompt ("What would you like the length of your password to be? Must be between 8 and 128 characters."))
-
+  // if statement to keep users from selecting a password that is too short or too long. 
     if (length < 8){
       alert('password to short')
     }
     if(length > 128){
       alert('password to long')
     }
+
+  // Showing the user some confirm messages in order to build their password to their desired spec. 
   var lowerCaseQ = confirm("Would you like to use lowercase letter?")
   var upperCaseQ = confirm("Would you like to use uppercase letters?")
   var numberQ = confirm("Would you like to use numbers?")
   var specCharQ = confirm("would you like to use special characters?")
 
+  // If statement making sure that the user selects at least one option. If not we will alert them that they must select at least one type.
+
   if (lowerCaseQ===false && upperCaseQ === false && numberQ === false && specCharQ === false){
 
     alert("Must select at least one type in order to generate password")
   }
+
+  // creating an object so that we can store the user inputs and pass them to the function below. 
 
   var answersObj = {
     lengthObj : length,
@@ -44,7 +49,7 @@ var result = [];
     specObj : specCharQ,
 
   }
-
+  // if statements that are reading stored customer inputs from the answersObj and generating results based on selections. 
   if (answersObj.upperObj){
       possibleChar = possibleChar.concat(upperCase);
       guranteeChar.push(random(upperCase));
@@ -56,14 +61,23 @@ var result = [];
 
 }
 
+  if (answersObj.specObj){
+  possibleChar = possibleChar.concat(symbol);
+  guranteeChar.push(random(symbol));
+}
+
+  if (answersObj.lowerObj){
+  possibleChar = possibleChar.concat(lowerCase);
+  guranteeChar.push(random(lowerCase));
+}
+
+
   for (let i = 0; i < answersObj.lengthObj; i++) {
     console.log(possibleChar)
     var res = random(possibleChar)
     result.push(res)
   }
 
-  console.log(result)
-  console.log(result.join(""))
   return result.join("")
 
 };
@@ -98,7 +112,6 @@ generateBtn.addEventListener("click", writePassword);
   // - as a series of confirms (answers are only true or false);
   // if all 4 confirms are false , alert use with " must select at least on character"
   // create an obj. 5 keys ( length, speachchar, numericchar, lowerchar, upperchar)
-
 
   //if obj.speachar  
   // var possChar = []
